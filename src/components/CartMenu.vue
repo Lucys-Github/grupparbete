2e8db3bc78f7915ad1e9fd450386524ba359b9e2
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-02-28 12:44:13 +0100

    Added more mockdata to store cart, Made a reusable component for cart items, fixed emit for cartCards

--- src/components/CartMenu.vue
+++ src/components/CartMenu.vue
@@ -1,27 +1,14 @@
 <script setup>
-import { ref, watch } from "vue";
-import { useCartStore } from "../store";
+import CartCards from "./CartCards.vue";
 
 const emit = defineEmits(["handle-cart-menu-activation"]);
 const props = defineProps({
   cartMenuActive: { required: true, type: Boolean },
 });
 
-const store = useCartStore();
-const cart = ref([]);
-
-if (store.cart !== null) {
-  cart.value = store.cart;
-  console.log(cart.value);
-}
-
-watch(
-  () => store.cart,
-  (newCart) => {
-    cart.value = newCart;
-    console.log(cart.value);
-  }
-);
+const handleCartMenuActivation = () => {
+  emit("handle-cart-menu-activation");
+};
 </script>
 
 <template>
@@ -54,35 +41,7 @@ watch(
       </div>
       <div id="bottom" class="flex flex-col h-full">
         <div class="flex-1 bg-[#1c1c1c] overflow-auto">
-          <div id="cartProducts" v-if="cart" v-for="product in cart">
-            <div class="p-2">
-              <div class="flex">
-                <img
-                  :src="product.imgSrc"
-                  :alt="product.alt"
-                  class="max-w-28"
-                />
-                <div class="flex flex-col flex-1 pl-5">
-                  <h2 class="font-semibold leading-1">
-                    {{ product.title }}
-                  </h2>
-                  <h3 class="text-xs font-thin pt-1">{{ product.brand }}</h3>
-                  <h3 class="text-sm font-light pt-2">
-                    {{ product.category }}
-                  </h3>
-                </div>
-                <div class="flex flex-col justify-between mx-3">
-                  <h2>{{ product.price }}:-</h2>
-                  <img
-                    src="/src/assets/icons/trash-icon.svg"
-                    alt="trashcan icon"
-                    class="h-7"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div v-else>Cart is empty</div>
+          <CartCards @handle-cart-menu-activation="handleCartMenuActivation" />
         </div>
         <div class="h-72 bg-[#0c0c0c] flex flex-col px-4">
           <div class="flex justify-between mt-6">
