b5f913bce4894833f228eb9f0a24714f859afb4f
Lucia <lucia.macakova@iths.se>
2024-02-29 13:01:04 +0100

    commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -21,7 +21,8 @@
       <!-- The card links to the product page, that in turn pass these props to the productInfo component on call -->
       <img :src="cardImgSrc" class="w-full h-32 sm:h-48 lg:h-56 object-cover" />
    </RouterLink>
-   <div><img src="/src/assets/icons/favorite-icon.svg" alt="red heart" @click="addToFavorites(product)"></div>
+<!--    text-[#FF007A] hover:fill-[#FF007A]
+ -->   <div @click="toggleFavorite(product)"><svg class="h-8 w-8 text-[#FF007A]" :class="{'fill-[#FF007A]': isFavorite}" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" /></svg></div>
   </div>
 
     <div
@@ -42,6 +43,7 @@
   <!-- card-background -->
 </template>
 <script>
+import { ref, onMounted} from "vue";
 export default {
   computed: {
     show() {
@@ -59,13 +61,41 @@ export default {
     },
   },
   setup() {
-    const addToFavorites = (product) => {
-      const favoritesArray = JSON.parse(localStorage.getItem("favoritesArray"))||[];
-     favoritesArray.push(product.title)
-    localStorage.setItem("favoritesArray", JSON.stringify(favoritesArray));
+    const isFavorite = ref();
+
+    const checkFavorite = (product) => {
+      let favoritesArray = JSON.parse(localStorage.getItem("favoritesArray"))||[];
+      if (favoritesArray.includes(product.title)){
+        return isFavorite.value = true
+      }
+      else{
+        return isFavorite.value = false
+      }
     }
 
-    return {addToFavorites}
+    onMounted(() => {
+      checkFavorite();
+    });
+
+    const toggleFavorite = (product) => {
+      let favoritesArray = JSON.parse(localStorage.getItem("favoritesArray"))||[];
+      if (favoritesArray.includes(product.title)){
+        favoritesArray = favoritesArray.filter((item) => item !== product.title);
+        isFavorite.value = false
+      }
+      else{
+        favoritesArray.push(product.title)
+        isFavorite.value = true
+      }
+
+      localStorage.setItem("favoritesArray", JSON.stringify(favoritesArray));
+
+      console.log(favoritesArray)
+      console.log(isFavorite)
+    }
+
+    return {toggleFavorite,
+    isFavorite, checkFavorite  }
   },
   props: {
     product: {
